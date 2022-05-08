f838549830da8317e6c32293025746b149063119
GaykarYash0309 <yashgaykar44@gmail.com>
2022-05-08 16:17:55 +0530

    MYSQL PROBLEMS SOLVED

---
+++ Problem11.sql
@@ -0,0 +1,5 @@
+-- Query the list of CITY names ending with vowels (a, e, i, o, u) from STATION. Your result cannot contain duplicates.
+SELECT DISTINCT CITY
+FROM STATION
+WHERE CITY REGEXP '[aeiou]$';
+-- SELECT DISTINCT CITY FROM STATION WHERE RIGHT(CITY,1) IN ('a','e','i','o','u');
