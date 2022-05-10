41e9558c8632ee1ca7968ec8bbead43f6002e167
GaykarYash0309 <yashgaykar44@gmail.com>
2022-05-10 22:07:58 +0530

    problem 25 solved

---
+++ Problem25.sql
@@ -0,0 +1,22 @@
+-- Amber's conglomerate corporation just acquired some new companies. Each of the companies follows this hierarchy: 
+-- Given the table schemas below, write a query to print the company_code, founder name, total number of lead managers,
+--  total number of senior managers, total number of managers, and total number of employees. Order your output by ascending
+--   company_code.
+SELECT c.company_code,
+    c.founder,
+    count(distinct l.lead_manager_code),
+    count(distinct s.senior_manager_code),
+    count(distinct m.manager_code),
+    count(distinct e.employee_code)
+FROM Company c,
+    Lead_Manager l,
+    Senior_Manager s,
+    Manager m,
+    Employee e
+where c.company_code = l.company_code
+    and l.lead_manager_code = s.lead_manager_code
+    and s.senior_manager_code = m.senior_manager_code
+    and m.manager_code = e.manager_code
+group by c.company_code,
+    c.founder
+ORDER BY c.company_code;
